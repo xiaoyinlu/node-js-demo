bbf1d64a2e2195092a0c4d1d57073c0f62da70b6
xiaoyinlu <halolxy@yeah.net>
2021-09-01 11:20:56 +0800

    2 files modified, 1 added

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-// run `node index.js` in the terminal
+// Require the framework and instantiate it
+const fastify = require('fastify')({
+  logger: true
+});
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+// Declare a route
+fastify.get('/', async (request, reply) => {
+  console.log(`!`);
+  return { hello: 'world' };
+});
 
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen(3000);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
